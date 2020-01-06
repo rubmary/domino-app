bcb47e98a784b373282c45183b00871fb3316fd7
rubmary <rubmaryrojas@gmail.com>
2020-01-05 23:55:18 -0400

    Statistics google cloud (#12)
    * Get statistics
    
    * fix bug
    
    * Update stats

--- server/api/statistics.py
+++ server/api/statistics.py
@@ -1,38 +1,30 @@
+from google.cloud import datastore
 from threading import Lock
 import json
 
 class Statistics:
     def __init__(self):
-        self.data = {
-            'player1': {
-                'average': 0,
-                'games': 0,
-                'sum': 0
-            },
-            'player2': {
-                'average': 0,
-                'games': 0,
-                'sum': 0
-            }
-        }
-        self.lock = Lock()
+        self.client = datastore.Client()
 
     def add_result(self, player1, player2, result):
-        with self.lock:
-            data = {}
+        query = self.client.query(kind='player', order=['id'])
+        results = list(query.fetch())
+        with self.client.transaction():
+            [stats_p1, stats_p2] = results
             if (player1):
-                stats = self.data['player1']
-                stats['games'] = stats['games'] + 1
-                stats['sum'] = stats['sum'] + result
-                stats['average'] = stats['sum']/stats['games']
+                stats_p1['games'] = stats_p1['games'] + 1
+                stats_p1['sum'] = stats_p1['sum'] + result
+                stats_p1['average'] = stats_p1['sum']/stats_p1['games']
             if (player2):
-                stats = self.data['player2']
-                stats['games'] = stats['games'] + 1
-                stats['sum'] = stats['sum'] - result
-                stats['average'] = stats['sum']/stats['games']
-        return "ok"
+                stats_p2['games'] = stats_p2['games'] + 1
+                stats_p2['sum'] = stats_p2['sum'] - result
+                stats_p2['average'] = stats_p2['sum']/stats_p2['games']
+            self.client.put_multi([stats_p1, stats_p2])
 
     def get_statistics(self):
-        with self.lock:
-            return self.data
-        return {"result": error}
+        query = self.client.query(kind='player', order=['id'])
+        results = list(query.fetch())
+        return {
+            'player1': results[0],
+            'player2': results[1]
+        }
