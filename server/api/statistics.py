2930b9d15ceb513bcd384e4dae5990f0c320c5b8
Rubmary Rojas <rubmaryrojas@gmail.com>
2020-01-05 23:50:48 -0400

    Update stats

--- server/api/statistics.py
+++ server/api/statistics.py
@@ -5,22 +5,21 @@ import json
 class Statistics:
     def __init__(self):
         self.client = datastore.Client()
-        self.lock = Lock()
 
     def add_result(self, player1, player2, result):
-        with self.lock:
-            data = {}
+        query = self.client.query(kind='player', order=['id'])
+        results = list(query.fetch())
+        with self.client.transaction():
+            [stats_p1, stats_p2] = results
             if (player1):
-                stats = self.data['player1']
-                stats['games'] = stats['games'] + 1
-                stats['sum'] = stats['sum'] + result
-                stats['average'] = stats['sum']/stats['games']
+                stats_p1['games'] = stats_p1['games'] + 1
+                stats_p1['sum'] = stats_p1['sum'] + result
+                stats_p1['average'] = stats_p1['sum']/stats_p1['games']
             if (player2):
-                stats = self.data['player2']
-                stats['games'] = stats['games'] + 1
-                stats['sum'] = stats['sum'] - result
-                stats['average'] = stats['sum']/stats['games']
-        return "ok"
+                stats_p2['games'] = stats_p2['games'] + 1
+                stats_p2['sum'] = stats_p2['sum'] - result
+                stats_p2['average'] = stats_p2['sum']/stats_p2['games']
+            self.client.put_multi([stats_p1, stats_p2])
 
     def get_statistics(self):
         query = self.client.query(kind='player', order=['id'])
